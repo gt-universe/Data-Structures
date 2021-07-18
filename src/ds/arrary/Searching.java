36c5b75da90d5f46bf447d25b91b6f59ca7482f9
Gaurav Tiwari <gaurav.tiwari@airtel.com>
2021-07-18 17:06:25 +0530

    Initial commit

---
+++ src/ds/arrary/Searching.java
@@ -0,0 +1,11 @@
+package ds.arrary;
+
+public class Searching {
+
+    private static String[] strArr;
+
+    public static void main(String[] args) {
+        strArr = new String[4];
+        System.out.print(strArr.length);
+    }
+}
